226bdf35c50c1761f63b4d8857294005613ade19
yubo.zhang <Nathaniel522880750@gmail.com>
2021-02-24 18:03:23 +0800

    JS练习

---
+++ cal.js
@@ -0,0 +1,13 @@
+// var x = "100";
+// var y = "10";
+// var z = x * y;
+// console.log(z);
+// console.log(z.isPrototypeOf(Object));
+let num = 2;
+let a = 0;
+while (num != Infinity){
+    a++;
+    num = num*num
+}
+console.log(a);
+console.log(2 ** 10);
--- operator.js
+++ operator.js
@@ -1,2 +1,9 @@
-document.getElementById("myTitle").innerHTML = "JavaScript效果展示";
-
+function zhanshi(){
+    document.getElementById("myTitle").innerHTML = "JavaScript效果展示";
+}
+function hide() {
+    document.getElementById("p1").style.display="none";
+}
+function show1() {
+    document.getElementById("p1").style.display="block";
+}
